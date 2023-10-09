3aac70b0608ee8f7ff960a82d21ffca437691dd0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-09 07:18:20 +0000

    Initial Commint

---
+++ lib/custom_code/widgets/index.dart
@@ -0,0 +1,3 @@
+export 'new_custom_widget.dart' show NewCustomWidget;
+export 'new_custom_widget2.dart' show NewCustomWidget2;
+export 'new_custom_widget3.dart' show NewCustomWidget3;
---
+++ lib/custom_code/widgets/new_custom_widget3.dart
@@ -0,0 +1,28 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class NewCustomWidget3 extends StatefulWidget {
+  const NewCustomWidget3({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+
+  @override
+  _NewCustomWidget3State createState() => _NewCustomWidget3State();
+}
+
+class _NewCustomWidget3State extends State<NewCustomWidget3> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
